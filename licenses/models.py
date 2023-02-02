f38c6e082f1abb7c4d8ffa732bb43218543c23c9
haycarlitos <carlos.castilloperera@gmail.com>
2023-02-02 15:29:18 -0800

    add models and migrations

--- licenses/models.py
+++ licenses/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=255)
+    contact_email = models.EmailField()
+
+class License(models.Model):
+    client = models.ForeignKey(
+        Client, on_delete=models.CASCADE, related_name='licenses'
+    )
+    license_id = models.CharField(max_length=255)
+    license_type = models.CharField(max_length=255)
+    package_name = models.CharField(max_length=255)
+    expiration_date = models.DateField()
